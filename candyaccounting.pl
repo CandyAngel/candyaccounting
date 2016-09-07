39baa479d47241fc135ee795373355cbc66b6fbe
CandyAngel <candyangel@electricjungle.org>
2016-09-07 11:03:13 +0100

    Change indentation

--- candyaccounting.pl
+++ candyaccounting.pl
@@ -1,15 +1,17 @@
 #!/usr/bin/env perl
+
 use Mojolicious::Lite;
 
 # Documentation browser under "/perldoc"
 plugin 'PODRenderer';
 
 get '/' => sub {
-  my $c = shift;
-  $c->render(template => 'index');
+	my $c = shift;
+	$c->render(template => 'index');
 };
 
 app->start;
+
 __DATA__
 
 @@ index.html.ep
@@ -22,6 +24,11 @@ To learn more, you can browse through the documentation
 @@ layouts/default.html.ep
 <!DOCTYPE html>
 <html>
-  <head><title><%= title %></title></head>
-  <body><%= content %></body>
+	<head>
+		<title><%= title %></title>
+	</head>
+
+	<body>
+<%= content %>
+	</body>
 </html>
